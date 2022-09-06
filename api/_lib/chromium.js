a7e8515920a21a0f8f8b79a24912e3b2c70ff0e2
ka66ann <ka66ann@gmail.com>
2022-09-06 04:10:37 +0300

    1 file added

---
+++ api/_lib/chromium.js
@@ -0,0 +1,22 @@
+import core from 'puppeteer-core';
+import { getOptions } from './options';
+let _page;
+async function getPage(isDev) {
+    if (_page) {
+        return _page;
+    }
+    const options = await getOptions(isDev);
+    const browser = await core.launch(options);
+    _page = await browser.newPage();
+    return _page;
+}
+export async function getScreenshot(html, type, isDev) {
+    const page = await getPage(isDev);
+    await page.setViewport({ width: 2048, height: 1170 });
+
+
+    console.log(html)
+    await page.setContent(html);
+    const file = await page.screenshot({ type });
+    return file;
+}
